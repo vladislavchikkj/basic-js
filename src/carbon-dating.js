274420266ff10517acd3ab5c54ad77640ff764b6
Vlad <vlad.kosevich@mail.ru>
2022-10-15 15:11:33 +0300

    add: carboundata

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+  if (sampleActivity && typeof sampleActivity == 'string' && sampleActivity>0 && sampleActivity < MODERN_ACTIVITY && isFinite(sampleActivity)==true) {  
+    let k =  0.693 / HALF_LIFE_PERIOD;
+    return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity) / k)
+  }
+  else {return false};
 }
 
 module.exports = {
